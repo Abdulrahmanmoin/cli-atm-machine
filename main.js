563b3adb44b50a5e92f5cefa1c052d2e5c7f1963
Abdulrahmanmoin <iam.armoin@gmail.com>
2024-03-29 16:12:36 +0500

    add chalk and correct the npm project name

--- main.js
+++ main.js
@@ -1,56 +1,57 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 let myBalance = 10000;
-console.log(`Your current balance is ${myBalance}`);
+console.log(chalk.magenta(`Your current balance is ${chalk.green(myBalance)}`));
 let pinAns = await inquirer.prompt([{
         name: "pin",
         type: "number",
-        message: "Enter your pin code:"
+        message: chalk.yellow("Enter your pin code:")
     }]);
 let myPin = 1234;
 if (myPin === pinAns.pin) {
-    console.log("Correct pin code.");
+    console.log(chalk.green("Correct pin code."));
     let operationAns = await inquirer.prompt([{
             name: "operation",
             type: "list",
-            message: "What you want to do?",
+            message: chalk.yellow("What you want to do?"),
             choices: ["Cash Withdrawal", "Fast Cash", "Check Balance", "Cash Deposit"]
         }]);
     if (operationAns.operation == "Cash Withdrawal") {
         let withdrawalAmount = await inquirer.prompt({
             name: "amount",
             type: "number",
-            message: "Enter amount:",
+            message: chalk.yellow("Enter amount:"),
         });
         if (withdrawalAmount.amount > myBalance) {
-            console.log("Insufficient Balance");
+            console.log(chalk.bold.red("Insufficient Balance"));
         }
         else {
-            console.log(`Your remaining amount is ${myBalance -= withdrawalAmount.amount}`);
+            console.log(chalk.blue(`Your remaining amount is ${myBalance -= withdrawalAmount.amount}`));
         }
     }
     else if (operationAns.operation == "Fast Cash") {
         let fastCashAns = await inquirer.prompt([{
                 name: "fastCash",
                 type: "list",
-                message: "Select your amount",
+                message: chalk.yellow("Select your amount"),
                 choices: [1000, 2000, 5000, 10000]
             }]);
-        console.log(`Your remaining balance is ${myBalance -= fastCashAns.fastCash}`);
+        console.log(chalk.blue(`Your remaining balance is ${myBalance -= fastCashAns.fastCash}`));
     }
     else if (operationAns.operation == "Cash Deposit") {
         let cashDepositAns = await inquirer.prompt([{
                 name: "cashDeposit",
                 type: "list",
-                message: "Select your amount to deposit",
+                message: chalk.yellow("Select your amount to deposit"),
                 choices: [1000, 2000, 5000, 10000]
             }]);
-        console.log(`Successfully deposit your cash. Now your current balance is ${myBalance += cashDepositAns.cashDeposit}`);
+        console.log(chalk.blue(`Successfully deposit your cash. Now your current balance is ${myBalance += cashDepositAns.cashDeposit}`));
     }
     else {
-        console.log(`Your current balance is ${myBalance}`);
+        console.log(chalk.cyanBright(`Your current balance is ${myBalance}`));
     }
 }
 else {
-    console.log("Wrong pin code.");
+    console.log(chalk.bold.red("Incorrect pin code."));
 }
